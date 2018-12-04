a6fbe309828c066e58290a47a1734dd76a5dfa6b
Dwayne Munro <dwaynemunro@Dwaynes-MBP.fios-router.home>
2018-12-03 23:14:27 -0500

    added heroku database

--- config/database.php
+++ config/database.php
@@ -42,11 +42,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '8889'),
+            'host' => env('DB_HOST', 'us-cdbr-iron-east-01.cleardb.net'),
+            'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'designstorm'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', 'root'),
+            'username' => env('DB_USERNAME', 'b7724f97937c34'),
+            'password' => env('DB_PASSWORD', '52deb75d'),
             'unix_socket' => env('DB_SOCKET', '/Applications/MAMP/tmp/mysql/mysql.sock'),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
